a5862fd6a779f2c3fda3988bc21f0162640547d8
Ubuntu <ubuntu@ip-172-31-29-253.ap-northeast-1.compute.internal>
2023-07-11 00:06:09 +0000

    Implement baic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,13 @@ class User < ApplicationRecord
                                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # わたさえrた文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                 BCript::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
 
